0f8186c526bc5c700c5fced4c21c7544964454c3
Poulpy <paul.repain@yahoo.com>
2020-03-04 18:58:00 +0100

    Simplify parse_args()

--- src/main.c
+++ src/main.c
@@ -51,9 +51,11 @@ int main(int argc, char *argv[])
 int parse_args(int argc, char *argv[], char **output_file, char **mat1_file,
                char **mat2_file)
 {
-    int good_input;
+    int good_input, no_out_file;
+    int order[3];
 
     good_input = 0;
+    no_out_file = 0;
 
     if (2 == argc)
     {
@@ -66,22 +68,16 @@ int parse_args(int argc, char *argv[], char **output_file, char **mat1_file,
     {
         if (strcmp(argv[1], "-o") == 0)
         {
-            *output_file = (char *) malloc(sizeof(char) * (strlen(argv[2]) + 1));
-            strcpy(*output_file, argv[2]);
-            *mat1_file = (char *) malloc(sizeof(char) * (strlen(argv[3]) + 1));
-            strcpy(*mat1_file, argv[3]);
-            *mat2_file = (char *) malloc(sizeof(char) * (strlen(argv[4]) + 1));
-            strcpy(*mat2_file, argv[4]);
+            order[0] = 2;
+            order[1] = 3;
+            order[2] = 4;
             good_input = 1;
         }
         else if (strcmp(argv[3], "-o") == 0)
         {
-            *output_file = (char *) malloc(sizeof(char) * (strlen(argv[4]) + 1));
-            strcpy(*output_file, argv[4]);
-            *mat1_file = (char *) malloc(sizeof(char) * (strlen(argv[1]) + 1));
-            strcpy(*mat1_file, argv[1]);
-            *mat2_file = (char *) malloc(sizeof(char) * (strlen(argv[2]) + 1));
-            strcpy(*mat2_file, argv[2]);
+            order[0] = 4;
+            order[1] = 1;
+            order[2] = 2;
             good_input = 1;
         }
         else
@@ -91,17 +87,32 @@ int parse_args(int argc, char *argv[], char **output_file, char **mat1_file,
     }
     else if (3 == argc)
     {
-        *output_file = (char *) malloc(sizeof(char) * (strlen(OUT_FILE) + 1));
-        strcpy(*output_file, OUT_FILE);
-        *mat1_file = (char *) malloc(sizeof(char) * (strlen(argv[1]) + 1));
-        strcpy(*mat1_file, argv[1]);
-        *mat2_file = (char *) malloc(sizeof(char) * (strlen(argv[2]) + 1));
-        strcpy(*mat2_file, argv[2]);
+        no_out_file = 1;
+        order[1] = 1;
+        order[2] = 2;
         good_input = 1;
     }
     else
     {
         print_help();
+    }
+
+    if (good_input)
+    {
+        if (no_out_file)
+        {
+            *output_file = (char *) malloc(sizeof(char) * (strlen(OUT_FILE) + 1));
+            strcpy(*output_file, OUT_FILE);
+        }
+        else
+        {
+            *output_file = (char *) malloc(sizeof(char) * (strlen(argv[order[0]]) + 1));
+            strcpy(*output_file, argv[order[0]]);
+        }
+        *mat1_file = (char *) malloc(sizeof(char) * (strlen(argv[order[1]]) + 1));
+        strcpy(*mat1_file, argv[order[1]]);
+        *mat2_file = (char *) malloc(sizeof(char) * (strlen(argv[order[2]]) + 1));
+        strcpy(*mat2_file, argv[order[2]]);
     }
 
     return good_input;
