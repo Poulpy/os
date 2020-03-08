d24c750bc02d438b02ffd3aeedfb7178190f4afb
Poulpy <paul.repain@yahoo.com>
2020-03-08 18:56:34 +0100

    Added some sleep

--- test/process.c
+++ test/process.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-#include<unistd.h>
+#include<stdlib.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<time.h>
+#include<unistd.h>
 
 #define TURNS 10
 
@@ -18,11 +20,13 @@ int main()
 
 void by_2(int *i)
 {
+    printf("%d * 2 = %d\n", (*i), *i * 2);
     *i *= 2;
 }
 
 void by_3(int *i)
 {
+    printf("%d * 3 = %d\n", *i, *i * 3);
     *i *= 3;
 }
 
@@ -33,9 +37,11 @@ void question1()
     void (*modify_var_by_2) (int *) = &by_2;
     void (*modify_var_by_3) (int *) = &by_3;
     int status;
+    int time_sleep;
 
     status = 0;
     child_pid = fork();
+    srand(getpid()%3000);
 
     if (child_pid == -1)
     {
@@ -46,9 +52,12 @@ void question1()
         printf("[fils] PID=%d PPID=%d g_var=%d\n", getpid(), getppid(), g_var);
         for (i = 0; i != TURNS; i++)
         {
-            printf("[fils] <%d> %d g_var=%d\n", getpid(), i, g_var);
             (*modify_var_by_2) (&g_var);
+            time_sleep = rand() % 2;
+            printf("[fils] <%d> %d g_var=%d sleep=%d\n", getpid(), i, g_var, time_sleep);
+            sleep(time_sleep);
         }
+        printf("[fils] Done\n");
         printf("*** End of process <%d> ***\n", getpid());
     }
     else /* This is the parent's thread */
@@ -56,10 +65,12 @@ void question1()
         printf("[pere] PID=%d PPID=%d g_var=%d\n", getpid(), getppid(), g_var);
         for (i = 0; i != TURNS; i++)
         {
-            printf("[pere] <%d> %d g_var=%d\n", getpid(), i, g_var);
             (*modify_var_by_3) (&g_var);
+            time_sleep = rand() % 5;
+            printf("[pere] <%d> %d g_var=%d sleep=%d\n", getpid(), i, g_var, time_sleep);
+            sleep(time_sleep);
         }
-        /*wait(NULL);*/
+        printf("[pere] Done\n");
         while ((wait_pid = wait(&status)) > 0);
         printf("*** End of process <%d> ***\n", getpid());
     }
