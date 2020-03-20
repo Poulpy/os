c7873d8b72ad87f472cf269d86883ecff9779bf7
Poulpy <paul.repain@yahoo.com>
2020-03-20 15:24:49 +0100

    Q2 : j'arrive po

--- test/thread.c
+++ test/thread.c
@@ -4,18 +4,20 @@
 
 void question11();
 void question12();
+void question12_mutex();
 
 #define TURNS 3
 #define P(x) sem_wait(x)
 #define V(x) sem_post(x)
 
 char g_var = 0;
-sem_t m1, m2;
+sem_t s1, s2;
+pthread_mutex_t m1, m2;
 
 int main()
 {
-    question11();
-    question12();
+    question12_mutex();
+
     return 0;
 }
 
@@ -52,20 +54,20 @@ void question11()
 void *print_jemes()
 {
     printf("Je ");
-    V(&m2);
-    P(&m1);
+    V(&s2);
+    P(&s1);
     printf("mes ");
-    V(&m2);
+    V(&s2);
 
     return NULL;
 }
 
 void *print_synchronizethreads()
 {
-    P(&m2);
+    P(&s2);
     printf("synchronize ");
-    V(&m1);
-    P(&m2);
+    V(&s1);
+    P(&s2);
     printf("threads\n");
 
     return NULL;
@@ -75,8 +77,8 @@ void question12()
 {
     pthread_t t1, t2;
 
-    sem_init(&m1, 0, 0);/* Initialize semaphore to 0 ! */
-    sem_init(&m2, 0, 0);
+    sem_init(&s1, 0, 0);/* Initialize semaphore to 0 ! */
+    sem_init(&s2, 0, 0);
 
     pthread_create(&t1, NULL, print_jemes, NULL);
     pthread_create(&t2, NULL, print_synchronizethreads, NULL);
@@ -84,7 +86,46 @@ void question12()
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
 
-    sem_destroy(&m1);
-    sem_destroy(&m2);
+    sem_destroy(&s1);
+    sem_destroy(&s2);
+}
+
+void *print_jemes_mutex()
+{
+    printf("Je ");
+    pthread_mutex_unlock(&m2);
+    pthread_mutex_lock(&m1);
+    printf("mes ");
+    pthread_mutex_unlock(&m2);
+
+    return NULL;
+}
+
+void *print_synchronizethreads_mutex()
+{
+    pthread_mutex_lock(&m2);
+    printf("synchronize ");
+    pthread_mutex_unlock(&m1);
+    pthread_mutex_lock(&m2);
+    printf("threads\n");
+
+    return NULL;
+}
+
+void question12_mutex()
+{
+    pthread_t t1, t2;
+
+    pthread_mutex_init(&m1, NULL);
+    pthread_mutex_init(&m2, NULL);
+
+    pthread_create(&t1, NULL, print_jemes_mutex, NULL);
+    pthread_create(&t2, NULL, print_synchronizethreads_mutex, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    pthread_mutex_destroy(&m1);
+    pthread_mutex_destroy(&m2);
 }
 
