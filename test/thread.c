9bfd549a3d692b041569af866f4e5bb41eb2a189
Poulpy <paul.repain@yahoo.com>
2020-03-20 13:55:32 +0100

    Q2 : using semaphores

--- test/thread.c
+++ test/thread.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 void question11();
+void question12();
 
 #define TURNS 3
+#define P(x) sem_wait(x)
+#define V(x) sem_post(x)
 
 char g_var = 0;
+sem_t m1, m2;
 
 int main()
 {
     question11();
+    question12();
     return 0;
 }
 
@@ -40,5 +46,45 @@ void question11()
     {
         pthread_join(threads[i], NULL);
     }
+}
+
+
+void *print_jemes()
+{
+    printf("Je ");
+    V(&m2);
+    P(&m1);
+    printf("mes ");
+    V(&m2);
+
+    return NULL;
+}
+
+void *print_synchronizethreads()
+{
+    P(&m2);
+    printf("synchronize ");
+    V(&m1);
+    P(&m2);
+    printf("threads\n");
+
+    return NULL;
+}
+
+void question12()
+{
+    pthread_t t1, t2;
+
+    sem_init(&m1, 0, 0);/* Initialize semaphore to 0 ! */
+    sem_init(&m2, 0, 0);
+
+    pthread_create(&t1, NULL, print_jemes, NULL);
+    pthread_create(&t2, NULL, print_synchronizethreads, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    sem_destroy(&m1);
+    sem_destroy(&m2);
 }
 
