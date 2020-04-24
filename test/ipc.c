f105e72215bda544a9d8d0886946c4bec8581f20
Poulpy <paul.repain@yahoo.com>
2020-04-24 15:59:08 +0200

    Ex 3 copy file

--- test/ipc.c
+++ test/ipc.c
@@ -80,9 +80,19 @@ void write_file(char *filename, char *to_write)
 {
     int fd_file_to_write;
 
-    fd_file_to_write = open(filename, O_CREAT | O_WRONLY | O_APPEND);
+    fd_file_to_write = open(filename, O_CREAT | O_WRONLY);
     write(fd_file_to_write, to_write, sizeof(char) * strlen(to_write));
     close(fd_file_to_write);
+}
+
+void copy_file(char *filename_tocopy, char *filename_topaste)
+{
+    char *content;
+
+    content = read_file(filename_tocopy);
+    write_file(filename_topaste, content);
+
+    free(content);
 }
 
 void question43()
