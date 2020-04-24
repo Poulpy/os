942ac66e1cec0b9add1500fba7367fd42ceea349
Poulpy <paul.repain@yahoo.com>
2020-04-24 13:21:23 +0200

    Exercise 1 : shell command that finds the standard streams filenumber

---
+++ test/ipc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Exercise 1
+ *
+ * Find the standard streams filenumber with a shell command :
+ *
+ * find /usr/include/* -type f | xargs grep -in "standard input"
+ * find /usr/include/* -type f | xargs grep -in "standard output"
+ *
+ */
+
