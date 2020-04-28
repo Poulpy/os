2e87825d6cbba20b0f29c27155282fb403044f33
Poulpy <paul.repain@yahoo.com>
2020-04-28 17:41:40 +0200

    Mystery number done ! :D Ex 5

--- test/ipc.c
+++ test/ipc.c
@@ -4,7 +4,14 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+#define MIN 10
+#define MAX 1000
+#define RIGHT 2
+#define LOWER 3
+#define GREATER 4
 
 /*
  * Exercise 1
@@ -64,7 +71,13 @@ char *read_file(char *filename)
         total_size = sizeof(char) * (strlen(file_content) + content_read + 1);
 
         if (strlen(tmp) != content_read)
+        {
+            printf("-------------------\n");
+            printf("strlen(tmp) %d\n", strlen(tmp));
+            printf("-------------------\n");
             tmp = (char *) realloc(tmp, sizeof(char) * (content_read));
+            printf("%s\n", tmp);
+        }
 
         file_content = (char *) realloc(file_content, total_size);
         strcat(file_content, tmp);
@@ -103,14 +116,152 @@ void question43()
     free(help_txt);
 }
 
+int status_tube[2] = { -1, -1 };
+int response_tube[2] = { -1, -1 };
+
+
+void send_to_father(int i)
+{
+    write(response_tube[1], &i, sizeof(int));
+}
+
+void send_to_child(int i)
+{
+    write(status_tube[1], &i, sizeof(int));
+}
+
+void close_tubes()
+{
+    close(response_tube[0]);
+    close(response_tube[1]);
+    close(status_tube[0]);
+    close(status_tube[1]);
+}
+
+int get_father_response()
+{
+    int i = 0;
+    read(status_tube[0], &i, sizeof(int));
+    return i;
+}
+
+int get_child_response()
+{
+    int i = 0;
+    read(response_tube[0], &i, sizeof(int));
+    return i;
+}
+
+void child()
+{
+    int guess, father_response;;
+    int lower_limit, upper_limit;
+    int has_guessed = 0;
+
+    lower_limit = MIN;
+    upper_limit = MAX;
+    srand(getpid());
+
+    while (!has_guessed)
+    {
+        guess = (rand() % (upper_limit - lower_limit)) + lower_limit;
+        /*guess = (upper_limit - lower_limit) / 2;*/
+        printf("[child] Is it %d ?\n", guess);
+
+        send_to_father(guess);
+
+        father_response = get_father_response();
+        printf("[child] Father said something !\n");
+
+        if (father_response == RIGHT)
+        {
+            printf("[child] Found ! It's %d\n", guess);
+            has_guessed = !has_guessed;
+        }
+        else if (father_response == LOWER)
+        {
+            printf("[child] It's lower than %d\n", guess);
+            upper_limit = guess;
+        }
+        else
+        {
+            printf("[child] It's greater than %d\n", guess);
+            lower_limit = guess;
+        }
+    }
+
+}
+
+void father()
+{
+    int to_guess, child_response;
+    int has_guessed = 0;
+    int status;
+    pid_t wait_pid;
+
+    srand(getpid());
+    to_guess = (rand() % (MAX - MIN)) + MIN;
+
+    printf("[father] The number to guess is %d\n", to_guess);
+
+    while (!has_guessed)
+    {
+        child_response = get_child_response();
+        printf("[father] You said %d\n", child_response);
+
+        if (child_response == to_guess)
+        {
+            has_guessed = !has_guessed;
+            send_to_child(RIGHT);
+        }
+        else if (child_response > to_guess)
+        {
+            send_to_child(LOWER);
+        }
+        else
+        {
+            send_to_child(GREATER);
+        }
+    }
+
+    while ((wait_pid = wait(&status)) > 0);
+}
+
+void open_tubes()
+{
+    if (pipe(status_tube) == -1 || pipe(response_tube) == -1)
+    {
+        perror("Damn... There's an error !\n");
+    }
+}
+
+void mystery_number()
+{
+    pid_t pid;
+
+    open_tubes();
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        perror("Couldn't create thread :/\n");
+    }
+    else if (pid == 0)
+    {
+        child();
+    }
+    else
+    {
+        father();
+    }
+
+    close_tubes();
+}
+
 int main(int argc, char *argv[])
 {
-    question41();
-
-    if (argc > 1)
-        question42(argv[1]);
-
-    question43();
+    mystery_number();
 
     return 0;
 }
